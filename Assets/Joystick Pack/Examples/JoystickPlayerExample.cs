749a6d1f8efc83e25329cf9dfd69e3c7349a876d
AbdullahY <yldzabdullah2003@gmail.com>
2023-11-21 13:08:49 +0300

    Revert "Revert "feat: Init the project""
    This reverts commit eb2e2b8dfef63f4a7b0258e78ad8bd709599854d.

---
+++ Assets/Joystick Pack/Examples/JoystickPlayerExample.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickPlayerExample : MonoBehaviour
+{
+    public float speed;
+    public VariableJoystick variableJoystick;
+    public CharacterController characterController;
+
+    public void FixedUpdate()
+    {
+        Vector3 direction = Vector3.forward * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
+        direction = transform.TransformDirection(direction);
+        characterController.Move(direction * speed * Time.fixedDeltaTime);
+    }
+}
